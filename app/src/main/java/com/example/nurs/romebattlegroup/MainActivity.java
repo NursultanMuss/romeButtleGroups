922700d1f030ac9127f55738fe497d49cd9a4d1f
NursultanMuss <nursalga@gmail.com>
2018-05-28 09:49:13 +0600

    правильная версия, добавил Loader

--- app/src/main/java/com/example/nurs/romebattlegroup/MainActivity.java
+++ app/src/main/java/com/example/nurs/romebattlegroup/MainActivity.java
@@ -1,23 +1,35 @@
 package com.example.nurs.romebattlegroup;
 
+import android.support.v4.app.LoaderManager;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.Adapter;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.example.nurs.romebattlegroup.data.DataAccess;
 import com.example.nurs.romebattlegroup.data.FractionsDbHelper;
 import com.example.nurs.romebattlegroup.data.MainFractionContract;
 
-public class MainActivity extends AppCompatActivity implements MainAdapter.MainAdapterOnClickHandler {
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class MainActivity extends AppCompatActivity implements MainAdapter.MainAdapterOnClickHandler, LoaderManager.LoaderCallbacks<Cursor> {
      RecyclerView mRecyclerView;
     private MainAdapter mMainAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    private SQLiteDatabase mDb;
+    private FractionsDbHelper mDb;
+    private Cursor fractions;
+    private DataAccess databaseAccess;
 
 
 
@@ -29,30 +41,68 @@ public class MainActivity extends AppCompatActivity implements MainAdapter.MainA
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
-        FractionsDbHelper dbHelper = new FractionsDbHelper(this);
-        mDb = dbHelper.getReadableDatabase();
-        Cursor cursor = readFromDb();
 
-        mMainAdapter = new MainAdapter(this, cursor);
+        //Do it in Backgrond Task
+
+        //end
+
+//        try{
+//
+//        }catch(IOException e){
+//
+//        }
+//        Cursor cursor = readFromDb();
+
+        mMainAdapter = new MainAdapter(this,this, fractions);
         mRecyclerView.setAdapter(mMainAdapter);
 
 
     }
 
     @Override
-    public void onClickListener(String str) {
+    public void onClickListener(String str) {}
 
-    }
-    private Cursor readFromDb(){
-        return mDb.query(
-                MainFractionContract.FractionsEntry.TABLE_NAME,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null
-        );
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.fractions_menu, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.web_page_link){
+            Uri uri= Uri.parse("https://www.honga.net/totalwar/rome2/faction.php?l=ru");
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            if(intent.resolveActivity(getPackageManager()) != null){
+                startActivity(intent);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this) {
+            @Override
+            public Cursor loadInBackground() {
+                databaseAccess = DataAccess.getInstance(MainActivity.this);
+                fractions = databaseAccess.getFractions();
+                databaseAccess.close();
+                return fractions;
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mMainAdapter.
+    }
+
+
+    @Override
+    public void onLoaderReset(Loader loader) {
+
+    }
 }
