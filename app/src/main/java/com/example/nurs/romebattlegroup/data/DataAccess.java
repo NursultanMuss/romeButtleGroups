3f863508e587a531da8fcb079f9cfa6ea88ff922
NursultanMuss <nursalga@gmail.com>
2018-05-28 09:57:05 +0600

    правильная версия, добавил Loader

---
+++ app/src/main/java/com/example/nurs/romebattlegroup/data/DataAccess.java
@@ -0,0 +1,64 @@
+package com.example.nurs.romebattlegroup.data;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+
+/**
+ * Created by nurs on 11.04.18.
+ */
+
+public class DataAccess {
+    private SQLiteOpenHelper openHelper;
+    private SQLiteDatabase database;
+    private static DataAccess instance;
+
+
+    private DataAccess (Context context){this.openHelper = new FractionsDbHelper(context);}
+    /**
+     * Return a singleton instance of DatabaseAccess.
+     *
+     * @param context the Context
+     * @return the instance of DabaseAccess
+     */
+    public static DataAccess getInstance(Context context){
+        if(instance == null){
+            instance = new DataAccess(context);
+        }
+        return instance;
+    }
+    /**
+     * Open the database connection.
+     */
+    public void open() {
+        this.database = openHelper.getWritableDatabase();
+    }
+
+    /**
+     * Close the database connection.
+     */
+    public void close() {
+        if (database != null) {
+            this.database.close();
+        }
+    }
+
+    public Cursor getFractions() {
+
+//        SQLiteDatabase db = getReadableDatabase();
+        open();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+        String [] sqlSelect = {"_id", "fraction_name", "img_res"};
+        String sqlTables = "fractions";
+
+        qb.setTables(sqlTables);
+        Cursor c = qb.query(this.database, sqlSelect, null, null,
+                null, null, null);
+        c.moveToFirst();
+        return c;
+
+    }
+}
--- app/src/main/java/com/example/nurs/romebattlegroup/data/FractionsDbHelper.java
+++ app/src/main/java/com/example/nurs/romebattlegroup/data/FractionsDbHelper.java
@@ -1,10 +1,20 @@
 package com.example.nurs.romebattlegroup.data;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.util.Log;
 
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.SQLException;
 
 /**
  * Created by nurs on 03.04.18.
@@ -12,22 +22,21 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class FractionsDbHelper extends SQLiteAssetHelper {
     public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "fractions.db";
+    public static final String DATABASE_NAME = "fractions.sqlite";
+    private static FractionsDbHelper instance;
+
+    private final String TAG = "Debug";
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public FractionsDbHelper (Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Log.d(TAG,"constructor called");
+        this.mContext = context;
     }
 
-//    @Override
-//    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-//
-//        sqLiteDatabase.execSQL(MainFractionContract.FractionsEntry.SQL_CREATE_ENTRIES);
-//    }
-//
-//    @Override
-//    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-//
-//        sqLiteDatabase.execSQL(MainFractionContract.FractionsEntry.SQL_DELETE_ENTRIES);
-//        onCreate(sqLiteDatabase);
-//    }
+
+
+
+
 }
